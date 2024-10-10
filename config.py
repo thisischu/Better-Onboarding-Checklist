3273f2367e8b0808ca23592ea11a7d34e3660c13
Thisischu <chuyilei1@gmail.com>
2024-10-10 16:40:45 -0400

    initial commit

---
+++ config.py
@@ -0,0 +1,9 @@
+# File paths
+input_file = 'OfferReport.xlsx'  # input file
+output_file = 'NewHire_IT_Checklist.xlsx'  # output file
+
+# Specify the start and end rows (1-based index)
+start_row = 327  # Start row 
+end_row = 351    # End row 
+
+#Run main.py file
